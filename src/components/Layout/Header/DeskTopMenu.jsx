996e431bcfb784f8ac974cf90e493cfd2a5b00ef
Noor <hi@noor.sh>
2022-06-04 00:10:02 +0530

    Update DeskTopMenu.jsx

--- src/components/Layout/Header/DeskTopMenu.jsx
+++ src/components/Layout/Header/DeskTopMenu.jsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  List,
   ListItem,
   Popover,
   Icon,
@@ -8,13 +9,20 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 import NavLink from "./NavLink";
-import DropDown from "./DropDown";
 
-export default function DeskTopMenu({ label, href, childrens }) {
+export default function DeskTopMenu({
+  label,
+  href,
+  childrens,
+  depthLevel,
+  closeNav,
+}) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  depthLevel = depthLevel + 1;
+  const placement = depthLevel > 1 ? "right-start" : "bottom";
 
   return (
     <ListItem>
@@ -25,19 +33,47 @@ export default function DeskTopMenu({ label, href, childrens }) {
           onClose={onClose}
           trigger="hover"
           gutter={0}
+          placement={placement}
         >
           <PopoverTrigger>
-            <Button p="1rem" variant="link">
+            <Button
+              py={depthLevel > 1 ? ".5rem" : "1rem"}
+              p="1rem"
+              w="100%"
+              justifyContent="space-between"
+              variant="link"
+            >
               {label}
-              <Icon as={ChevronDownIcon} w={5} h={5} ml={1} />
+              <Icon
+                as={depthLevel > 1 ? ChevronRightIcon : ChevronDownIcon}
+                w={5}
+                h={5}
+                ml={1}
+              />
             </Button>
           </PopoverTrigger>
           <PopoverContent width="inherit" minW="10rem" bg="white">
-            <DropDown closeNav={onClose} childrens={childrens} />
+            <List
+              px={{ base: "1rem", md: "0" }}
+              borderLeft={{ base: "1px", md: "0" }}
+              borderColor="gray.200"
+            >
+              {childrens.map((childdata, index) => (
+                <ListItem key={index}>
+                  <DeskTopMenu
+                    {...childdata}
+                    depthLevel={depthLevel}
+                    closeNav={onClose}
+                  />
+                </ListItem>
+              ))}
+            </List>
           </PopoverContent>
         </Popover>
       ) : (
-        <NavLink href={href}>{label}</NavLink>
+        <NavLink href={href} closeNav={closeNav}>
+          {label}
+        </NavLink>
       )}
     </ListItem>
   );
