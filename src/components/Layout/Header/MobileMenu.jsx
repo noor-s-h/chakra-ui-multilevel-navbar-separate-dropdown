adf3c80d5bdb25e5cc8eb2289b6692d233a1d759
Noor <hi@noor.sh>
2022-06-05 16:18:54 +0530

    Update MobileMenu.jsx

--- src/components/Layout/Header/MobileMenu.jsx
+++ src/components/Layout/Header/MobileMenu.jsx
@@ -1,42 +1,17 @@
-import {
-  Button,
-  List,
-  ListItem,
-  Collapse,
-  Icon,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { ListItem } from '@chakra-ui/react';
 
-import { ChevronDownIcon } from "@chakra-ui/icons";
-
-import NavLink from "./NavLink";
+import NavLink from './NavLink';
+import MobileDropDown from './MobileDropDown';
 
 export default function MobileMenu({ label, href, childrens, closeNav }) {
-  const { isOpen, onToggle } = useDisclosure();
-
   return (
     <ListItem>
       {childrens ? (
-        <>
-          <Button w="100%" py=".5rem" variant="link" onClick={onToggle}>
-            {label}
-            <Icon
-              as={ChevronDownIcon}
-              transition="all .25s ease-in-out"
-              transform={isOpen ? "rotate(180deg)" : ""}
-              w={6}
-              h={6}
-              ml="auto"
-            />
-          </Button>
-          <Collapse in={isOpen} unmountOnExit>
-            <List pl="1rem" borderLeft="1px" borderColor="gray.200">
-              {childrens.map((childdata, index) => (
-                  <MobileMenu key={index} {...childdata} closeNav={closeNav} />
-              ))}
-            </List>
-          </Collapse>
-        </>
+        <MobileDropDown
+          label={label}
+          childrens={childrens}
+          closeNav={closeNav}
+        />
       ) : (
         <NavLink href={href} closeNav={closeNav}>
           {label}
